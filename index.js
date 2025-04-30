4e5dd1dd8b011b98a993a1c165057303213a2aae
Ivaz <ivazreza@gmail.com>
2025-04-30 16:23:16 +0700

    chore: install mongoose to use schema

--- index.js
+++ index.js
@@ -2,23 +2,35 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient } = require('mongodb')
+const mongoose = require('mongoose')
+const { Schema } = mongoose
 
-// configuration
+// database config
+const UserSchema = new Schema({
+  username: String
+})
+const ExerciseSchema = new Schema({
+  user_id: {
+    type: String,
+    required: true
+  },
+  description: String,
+  duration: Number,
+  date: Date
+})
+
+const user = mongoose.model("User", UserSchema)
+const exercise = mongoose.model("Exercise", ExerciseSchema)
+
+// basic config
 app.use(cors())
 app.use(express.static('public'))
-const client = new MongoClient(process.env.DB_URL)
-const db = client.db("exerciseTracker")
-const users = db.collection("users")
-const exercise = db.collection("exercise")
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 
 // api request
-
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
