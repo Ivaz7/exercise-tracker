f03ee5004e79e3c4bd3be1e0ae15b627fafaa58c
Ivaz <ivazreza@gmail.com>
2025-04-30 18:12:43 +0700

    fix: index html

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(cors())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/public/index.html')
 });
 
 
