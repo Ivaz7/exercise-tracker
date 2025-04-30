c46b220637728ed12bbb6981e062ee8bce66ed77
Ivaz <ivazreza@gmail.com>
2025-04-30 16:08:13 +0700

    chore: install mongodb and create collection and database

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient } = require('mongodb')
 
+// configuration
 app.use(cors())
 app.use(express.static('public'))
+const client = new MongoClient(process.env.DB_URL)
+const db = client.db("exerciseTracker")
+const users = db.collection("users")
+const exercise = db.collection("exercise")
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 
+// api request
 
 
 
