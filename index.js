2cbb5d1acdabbce7bc16900c2b01705ee839bbde
Ivaz <ivazreza@gmail.com>
2025-04-30 16:37:07 +0700

    feat: add post request to create user

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 // database config
+mongoose.connect(process.env.DB_URL)
+
 const UserSchema = new Schema({
   username: String
 })
@@ -19,19 +21,35 @@ const ExerciseSchema = new Schema({
   date: Date
 })
 
-const user = mongoose.model("User", UserSchema)
-const exercise = mongoose.model("Exercise", ExerciseSchema)
+const User = mongoose.model("User", UserSchema)
+const Exercise = mongoose.model("Exercise", ExerciseSchema)
 
 // basic config
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 
 // api request
+// create user
+app.post('/api/users', async (req, res) => {
+  const { username } = req.body
+  const userObj = new User({
+    username,
+  })
 
+  try {
+    const user = await userObj.save()
+    res.json(user)
+  } catch (err) {
+    res.json({
+      error: err
+    })
+  }
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
